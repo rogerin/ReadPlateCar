b1f512d18017ed62c6ec0f9d4c438122cae3b5bc
Rogerio Alencar Filho <geriofilho@gmail.com>
2020-11-17 15:21:29 -0300

    read last line

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 /*
  DOC
  command: v4l2-ctl --list-devices
- alpr /dev/video0 -j stdin > /home/pi/Documents/readplate/public/camera1.json 
- alpr /dev/video2 -j stdin > /home/pi/Documents/readplate/public/camera2.json 
- alpr /dev/video4 -j stdin > /home/pi/Documents/readplate/public/camera3.json 
- alpr /dev/video6 -j stdin > /home/pi/Documents/readplate/public/camera4.json 
+ alpr /dev/video0 -c br -j stdin > /home/pi/Documents/readplate/public/camera1.json 
+ alpr /dev/video2 -c vr -j stdin > /home/pi/Documents/readplate/public/camera2.json 
+ alpr /dev/video4 -c vr -j stdin > /home/pi/Documents/readplate/public/camera3.json 
+ alpr /dev/video6 -c vr -j stdin > /home/pi/Documents/readplate/public/camera4.json 
  
 
 */
@@ -15,7 +16,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+const readLastLines = require('read-last-lines');
 
 const myEmitter = new EventEmitter();
 let connected = false;
@@ -24,6 +25,7 @@ io.on("connection", function (client) {
     connected = true;
     
     myEmitter.on('event', function(data, camera) {
+        console.log('send', data)
         if(connected) {
             client.emit("event", data.results[0]);
         }
@@ -42,17 +44,24 @@ let read = true;
 fs.watch(paths.camera1, function (event, filename) {
     if (filename) {
         try {
-            fs.readFile(filename, function(error, data){
-                try {
-                    let value = JSON.parse(data);
-                    
-                    if(value.results.length) {
-                        if(value.results[0].confidence >= 79 ) {
-                            if(value.results[0].confidence >= 78.5) myEmitter.emit('event', value)
+            readLastLines.read(paths.camera1, 1)
+                .then((line) => {
+                    console.log(line)
+                    try {
+                        let value = JSON.parse(line);
+                        console.log(value)
+                        if(value.results.length) {
+                            if(value.results[0].confidence >= 79 ) {
+                                if(value.results[0].confidence >= 78.5) {
+                                    myEmitter.emit('event', value)
+                                }
+                            }
                         }
-                    }
-                } catch (error) { }
-            })
+                    } catch (error) {  console.log(error)}
+                });
+
+                
+          
         } catch (error) {
             console.log(error)
         }
@@ -65,17 +74,24 @@ fs.watch(paths.camera1, function (event, filename) {
 fs.watch(paths.camera2, function (event, filename) {
     if (filename) {
         try {
-            fs.readFile(filename, function(error, data){
-                try {
-                    let value = JSON.parse(data);
-                    
-                    if(value.results.length) {
-                        if(value.results[0].confidence >= 79 ) {
-                            if(value.results[0].confidence >= 78.5) myEmitter.emit('event', value)
+            readLastLines.read(paths.camera2, 1)
+                .then((line) => {
+                    console.log(line)
+                    try {
+                        let value = JSON.parse(line);
+                        console.log(value)
+                        if(value.results.length) {
+                            if(value.results[0].confidence >= 79 ) {
+                                if(value.results[0].confidence >= 78.5) {
+                                    myEmitter.emit('event', value)
+                                }
+                            }
                         }
-                    }
-                } catch (error) { }
-            })
+                    } catch (error) {  console.log(error)}
+                });
+
+                
+          
         } catch (error) {
             console.log(error)
         }
@@ -86,21 +102,26 @@ fs.watch(paths.camera2, function (event, filename) {
 });
 
 fs.watch(paths.camera3, function (event, filename) {
-    
     if (filename) {
-        console.log(paths.camera3)
         try {
-            fs.readFile(filename, function(error, data){
-                try {
-                    let value = JSON.parse(data);
-                    
-                    if(value.results.length) {
-                        if(value.results[0].confidence >= 79 ) {
-                            if(value.results[0].confidence >= 78.5) myEmitter.emit('event', value)
+            readLastLines.read(paths.camera3, 1)
+                .then((line) => {
+                    console.log(line)
+                    try {
+                        let value = JSON.parse(line);
+                        console.log(value)
+                        if(value.results.length) {
+                            if(value.results[0].confidence >= 79 ) {
+                                if(value.results[0].confidence >= 78.5) {
+                                    myEmitter.emit('event', value)
+                                }
+                            }
                         }
-                    }
-                } catch (error) { }
-            })
+                    } catch (error) {  console.log(error)}
+                });
+
+                
+          
         } catch (error) {
             console.log(error)
         }
@@ -112,19 +133,25 @@ fs.watch(paths.camera3, function (event, filename) {
 
 fs.watch(paths.camera4, function (event, filename) {
     if (filename) {
-        console.log(paths.camera4)
         try {
-            fs.readFile(filename, function(error, data){
-                try {
-                    let value = JSON.parse(data);
-                    
-                    if(value.results.length) {
-                        if(value.results[0].confidence >= 79 ) {
-                            if(value.results[0].confidence >= 78.5) myEmitter.emit('event', value)
+            readLastLines.read(paths.camera4, 1)
+                .then((line) => {
+                    console.log(line)
+                    try {
+                        let value = JSON.parse(line);
+                        console.log(value)
+                        if(value.results.length) {
+                            if(value.results[0].confidence >= 79 ) {
+                                if(value.results[0].confidence >= 78.5) {
+                                    myEmitter.emit('event', value)
+                                }
+                            }
                         }
-                    }
-                } catch (error) { }
-            })
+                    } catch (error) {  console.log(error)}
+                });
+
+                
+          
         } catch (error) {
             console.log(error)
         }
@@ -133,6 +160,7 @@ fs.watch(paths.camera4, function (event, filename) {
         console.log('filename not provided');
     }
 });
+
 
 http.listen(3000, function(){
     console.log('listening on port 3000');
